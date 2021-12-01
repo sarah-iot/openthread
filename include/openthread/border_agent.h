b49999ee98606d5c9fef6dc2d8329bde97e34bab
Sarah <91494325+sarah-iot@users.noreply.github.com>
2021-12-01 13:11:08 -0700

    Update border_agent.h

--- include/openthread/border_agent.h
+++ include/openthread/border_agent.h
@@ -73,7 +73,13 @@ typedef enum otBorderAgentState
 otBorderAgentState otBorderAgentGetState(otInstance *aInstance);
 
 /**
- * @cli baport
+ * @section sec An example section
+ * This page contains the subsections \ref subsection1 and \ref subsection2.
+ * For more info see page \ref page2.
+ * @subsection subsection1 The first subsection
+ * Text.
+ * @subsection subsection2 The second subsection
+ * More text.
  * 
  * @code
  * > ba port
