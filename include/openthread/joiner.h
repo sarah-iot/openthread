4ea20df27b298eb85bbe53bddb45308a6e575839
Sarah Brenner <sarahca@google.com>
2022-01-19 09:58:38 -0700

    cmd updates

--- include/openthread/joiner.h
+++ include/openthread/joiner.h
@@ -179,7 +179,7 @@ const otExtAddress *otJoinerGetId(otInstance *aInstance);
  * joiner discerner clear
  * Done
  * @endcode
- * @cparams joiner discerner \<discerner\>
+ * @cparams joiner discerner discerner
  * Use `{number}/{length}` to set the `discerner`.
  * @par
  * `joiner discerner clear` sets `aDiscerner` to `nullptr`.
